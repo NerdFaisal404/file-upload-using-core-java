cc24863774ce4fbe0099cc10ef1e12b3502ce3cf
faisalahmed <faisal.ahmedbcse@gmail.com>
2022-01-09 00:25:03 +0600

    curl rnd done

---
+++ src/main/java/CurlRequest.java
@@ -0,0 +1,34 @@
+import com.sun.deploy.util.StringUtils;
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class CurlRequest {
+    public static void main(String[] args) throws IOException {
+//        String command =
+//                "curl -c logincookie.txt --data 'username=geneakin_5bb&password=0f23aae2' https://portal.proxyguys.com/login";
+//        ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
+//        processBuilder.directory(new File("/Users/faisalahmed/Desktop/"));
+//        Process process = processBuilder.start();
+//        InputStream inputStream = process.getInputStream();
+//
+//        System.out.println( IOUtils.toString( inputStream , "UTF-8") );
+
+        try (InputStream in = new FileInputStream("/Users/faisalahmed/Desktop/logincookie.txt")) {
+            System.out.println(IOUtils.toString(in, "UTF-8"));
+            String cookieValue = IOUtils.toString(in, "UTF-8");
+
+            URL url = new URL("https://portal.proxyguys.com/api/v2/proxies/availability");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Cookie",
+                    "session=79558b8e-6a52-4e83-a3f7-180c3d8539c1.kvU1QsFYV3aZo1zoZdhYH1vShzE");
+            int status = con.getResponseCode();
+
+            System.out.println(IOUtils.toString(con.getInputStream(), "UTF-8"));
+            con.disconnect();
+        }
+    }
+}
